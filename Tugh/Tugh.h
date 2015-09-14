2f3436900131d04db69530687f1e0afa32f12209
Robert Otani <robert@otanistudio.com>
2015-09-14 14:28:39 -0700

    Initial Commit

---
+++ Tugh/Tugh.h
@@ -0,0 +1,19 @@
+//
+//  Tugh.h
+//  Tugh
+//
+//  Created by Robert Otani on 9/14/15.
+//  Copyright © 2015 otanistudio.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Tugh.
+FOUNDATION_EXPORT double TughVersionNumber;
+
+//! Project version string for Tugh.
+FOUNDATION_EXPORT const unsigned char TughVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tugh/PublicHeader.h>
+
+
