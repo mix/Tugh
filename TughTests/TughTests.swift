6543229c5a4b469e93d0e583fa0f64203817e8ba
Robert Otani <robert@otanistudio.com>
2015-09-15 16:34:05 -0700

    Support for adding a callback url

--- TughTests/TughTests.swift
+++ TughTests/TughTests.swift
@@ -9,21 +9,19 @@
 import XCTest
 @testable import Tugh
 
-
-
 class TughTests: XCTestCase {
-    func testTokenAuthHeader() {
-        
-        // Override random/timestamp stuff for testability
-        class TughTest : Tugh {
-            override private class func nonce() -> String {
-                return "AlwaysTheSameNonceForTesting"
-            }
-            override private class func timestamp() -> String {
-                return "1442358472"
-            }
+    
+    class TughTest : Tugh {
+        // Override random/timestamp stuff for testability, only use when you really need it in the tests
+        override internal class func nonce() -> String {
+            return "AlwaysTheSameNonceForTesting"
         }
-        
+        override internal class func timestamp() -> String {
+            return "1442358472"
+        }
+    }
+    
+    func testTokenAuthHeader() {
         // These keys are from the example code in Twitter documentation, and are useless outside of that context
         let appKey = "kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw"
         let appSecret = "LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE"
@@ -36,8 +34,22 @@ class TughTests: XCTestCase {
         
         let expectedHeaderValue = "OAuth oauth_signature_method=\"HMAC-SHA1\", oauth_version=\"1.0\", oauth_timestamp=\"1442358472\", oauth_signature=\"G1qKuDVd1VXDu5d1%2BBkM2IjTPtw%3D\", oauth_nonce=\"AlwaysTheSameNonceForTesting\", oauth_consumer_key=\"kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw\""
 
-        debugPrint(authHeader)
         XCTAssertEqual(expectedHeaderValue, authHeader)
+    }
+    
+    func testAddCallbackToAuthGenerator() {
+        let appKey = "whocaresaboutthisappKeyIDO"
+        let appSecret = "thisIsTOTALLYASecretPeople"
+        
+        let authHeader = TughTest.twitterOAuthTokenAuthHeader(
+            "http://example.com/oauth/something",
+            appKey: appKey,
+            appSecret: appSecret,
+            callbackURI: "tugh://turn/down/for/wat")
+        
+        let expectedResult = "OAuth oauth_consumer_key=\"whocaresaboutthisappKeyIDO\", oauth_callback=\"tugh%3A%2F%2Fturn%2Fdown%2Ffor%2Fwat\", oauth_signature=\"zCUA46aJNrv6u7pUqbDCpYIGYcA%3D\", oauth_version=\"1.0\", oauth_timestamp=\"1442358472\", oauth_nonce=\"AlwaysTheSameNonceForTesting\", oauth_signature_method=\"HMAC-SHA1\""
+        
+        XCTAssertEqual(expectedResult, authHeader)
     }
 
 }
