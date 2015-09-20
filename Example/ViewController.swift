aa2d85d844fa81a9b51f6a2ef0fa4ad85af42562
Robert Otani <robert@otanistudio.com>
2015-09-20 14:41:43 -0700

    wire-up outlets

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var consumerKeyField: UITextField!
+    @IBOutlet weak var consumerSecretField: UITextField!
+    @IBOutlet weak var clearButton: UIButton!
+    @IBOutlet weak var reverseOAuthButton: UIButton!
+    @IBOutlet weak var oAuthButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
     }
+    
+    
+    
 
 
 
